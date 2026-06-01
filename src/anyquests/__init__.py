90ecf5c344ddb19d4e342c5ce1ef80fa2d5264fe
abel1502 <terragame.abel@gmail.com>
2026-06-01 11:14:45 +0200

    Implement package

--- src/anyquests/__init__.py
+++ src/anyquests/__init__.py
@@ -1,1 +1,23 @@
+import sys
+import textwrap
 
+try:
+    import niquests
+    sys.modules["anyquests"] = niquests
+except ImportError:
+    try:
+        import requests
+        sys.modules["anyquests"] = requests
+    except ImportError:
+        raise ImportError(textwrap.dedent(
+            """
+            Neither niquests nor requests are installed. Please install one of these package and try again.
+            
+            Use:
+                pip install niquests
+            or:
+                pip install requests
+            
+            For more information see: https://github.com/niquests/anyquests#i-am-an-end-user-which-one-to-choose
+            """
+        )) from None
